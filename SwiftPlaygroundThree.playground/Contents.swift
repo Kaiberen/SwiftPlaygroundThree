87c8a4c05df3727f18b7f919424ae3fd28cbf8b6
Kaiberenn <Kaiberen@hawaii.edu>
2019-02-14 10:46:26 -1000

    Assingment #4 Complete

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -29,3 +29,25 @@ print(fruitNames[1])
 print(fruitNames[0])
 print(fruitNames[2])
 
+//Assignment #4 Arrays Part 2: Instance Methods
+//Part 8, Start of Assignment #4
+
+var myNumbers: [Int] = [7, 11, 4, 8, 12]
+
+print(myNumbers)
+
+print("My favorite number is\(myNumbers)")
+
+for name in myNumbers
+{
+    print ("My favorite number is", (name))
+}
+
+print(myNumbers[1])
+print(myNumbers[0])
+print(myNumbers[2])
+print(myNumbers[3])
+print(myNumbers[4])
+
+
+

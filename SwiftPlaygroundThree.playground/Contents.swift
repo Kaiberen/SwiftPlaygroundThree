cb63f4c08e49d2b7068afd5cb0d2cb883891bcbe
Kaiberenn <Kaiberen@hawaii.edu>
2019-02-14 10:30:37 -1000

    Intro to Arrays

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Part 2
+//Declaration
+
+var fruitNames: [String] = ["Apple", "Orange", "Banana"]
+
+//Assign Fruit Names with three favorite fruits
+//Part 3
+
+//Part 4
+print(fruitNames)
+
+//Part 5
+print("I Like to eat\(fruitNames)")
+
+//Part 6
+for name in fruitNames
+{
+    print("I like to eat" + name)
+    
+}
+
+//Part 7
+//print "name" at index 1 in fruitname
+print(fruitNames[1])
+print(fruitNames[0])
+print(fruitNames[2])
+
